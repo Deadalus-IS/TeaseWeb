6f086b4d3837aa83aa8f44d6d5d602cc2c85185b
champ3oy <akotosel6@gmail.com>
2022-01-02 10:32:53 +0000

    card

--- components/Head.js
+++ components/Head.js
@@ -13,18 +13,19 @@ export default function Headd({ title, image }) {
         content={title + " | Discover and Manage events"}
       ></meta>
       <meta
-        content={title + " | Discover and Manage events"}
-        property="og:title"
-      ></meta>
-      <meta
-        property="twitter:title"
-        content={title + " | Discover and Manage events"}
+        property="og:image"
+        content={image ? image : "https://teaseafrica.netlify.app/1.png"}
+        data-vue-meta="true"
       ></meta>
 
       <meta property="og:type" content="website"></meta>
       <meta property="og:url" content="https://tease.africa/"></meta>
       <meta
         name="description"
+        content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
+      />
+      <meta
+        name="og:description"
         content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
       />
       <link rel="icon" href="/icon.png" />
@@ -39,31 +40,20 @@ export default function Headd({ title, image }) {
       ></meta>
 
       <meta
-        content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
-        property="twitter:description"
-      ></meta>
-      <meta
-        property="twitter:title"
+        name="twitter:title"
         content={title + " | Discover and Manage events"}
       ></meta>
-      <meta property="twitter:url" content="https://tease.africa/"></meta>
       <meta
-        property="twitter:card"
-        content={image ? image : "https://teaseafrica.netlify.app/1.png"}
+        content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
+        name="twitter:description"
       ></meta>
+      <meta name="twitter:url" content="https://tease.africa/"></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:site" content="@TeaseAfrica"></meta>
+      <meta name="twitter:creator" content="@TeaseAfrica"></meta>
       <meta
-        property="twitter:image"
+        name="twitter:image"
         content={image ? image : "https://teaseafrica.netlify.app/1.png"}
-      ></meta>
-      <meta
-        property="twitter:image"
-        content={image ? image : "https://teaseafrica.netlify.app/1.png"}
-        data-vue-meta="true"
-      ></meta>
-      <meta
-        property="og:image"
-        content={image ? image : "https://teaseafrica.netlify.app/1.png"}
-        data-vue-meta="true"
       ></meta>
 
       <script
