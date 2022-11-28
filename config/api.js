a56732a71f686879a9a186be0ba18dbc614326bb
champ3oy <akotosel6@gmail.com>
2022-11-28 17:59:47 +0000

    add: backup server running on render.com

--- config/api.js
+++ config/api.js
@@ -1,3 +1,5 @@
 export const BASE_URL = "https://passticketgo.herokuapp.com/api";
 export const BASE_URL_ADMIN = "https://passticketgo.herokuapp.com/admin";
 // const BASE_URL = "http://localhost:39637/api";
+
+// second server https://tease-backend.onrender.com
