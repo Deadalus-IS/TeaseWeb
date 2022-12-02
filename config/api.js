32c7a9dbf411820098818bf703c6b601c97aab69
champ3oy <akotosel6@gmail.com>
2022-12-02 02:19:24 +0000

    fix: switch to render.com servers

--- config/api.js
+++ config/api.js
@@ -1,5 +1,8 @@
-export const BASE_URL = "https://passticketgo.herokuapp.com/api";
-export const BASE_URL_ADMIN = "https://passticketgo.herokuapp.com/admin";
+// export const BASE_URL = "https://passticketgo.herokuapp.com/api";
+// export const BASE_URL_ADMIN = "https://passticketgo.herokuapp.com/admin";
 // const BASE_URL = "http://localhost:39637/api";
 
 // second server https://tease-backend.onrender.com
+
+export const BASE_URL = "https://tease-backend.onrender.com/api";
+export const BASE_URL_ADMIN = "https://tease-backend.onrender.com/admin";
