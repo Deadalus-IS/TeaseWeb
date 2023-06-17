caaaeb43f7323438ded7884ee2c4a1c5db4435ae
champ3oy <akotosel6@gmail.com>
2023-06-17 10:27:32 +0000

    mod: fixes

--- config/api.js
+++ config/api.js
@@ -1,8 +1,8 @@
 // export const BASE_URL = "https://tease-backend.onrender.com/api";
-export const BASE_URL_ADMIN = "http://localhost:54443/admin";
-export const BASE_URL = "http://localhost:54443/api";
+// export const BASE_URL_ADMIN = "http://localhost:54443/admin";
+// export const BASE_URL = "http://localhost:54443/api";
 
 // second server https://tease-backend.onrender.com
 
-// export const BASE_URL = "https://tease-backend.onrender.com/api";
-// export const BASE_URL_ADMIN = "https://tease-backend.onrender.com/admin";
+export const BASE_URL = "https://tease-backend.onrender.com/api";
+export const BASE_URL_ADMIN = "https://tease-backend.onrender.com/admin";
