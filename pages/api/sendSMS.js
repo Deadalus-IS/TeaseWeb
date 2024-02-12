35fd9008578ab3210f9e020cefba8910e33c7c06
Cirlorm <akotosel6@gmail.com>
2024-02-12 11:43:34 +0000

    mod: fix

--- pages/api/sendSMS.js
+++ pages/api/sendSMS.js
@@ -1,6 +1,27 @@
 // pages/api/sendMessage.js
+import Cors from "cors";
+
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  Cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 
 export default async function handler(req, res) {
+  await cors(req, res);
   if (req.method === "GET") {
     const { phoneNumber, message } = req.query;
 
